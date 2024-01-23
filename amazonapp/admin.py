8f843955ee24bffa51a3b1f84280be8b1e894e7b
RISWINMOIDU <riswinmoid.m@gmail.com.com>
2024-01-23 10:21:11 +0530

    first commit

---
+++ amazonapp/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from . models import Department,Doctors,booking
+
+# Register your models here.
+ 
+admin.site.register(Department)
+admin.site.register(Doctors)
+class BookingAdmin(admin.ModelAdmin):
+    list_display=("id","p_phone","doc_name","booking_date","booked_on")
+admin.site.register(booking,BookingAdmin)
+
